39441ddc747ecdeef605d4dc6afd2c7741a351b9
Shuning <shuning@aukilabs.com>
2024-10-14 12:46:34 +0800

    first version of networking module working on both native and browser

--- networking/src/context.rs
+++ networking/src/context.rs
@@ -1,1 +1,68 @@
-pub struct Context { }
+use crate::network::{NetworkingConfig, RNetworking};
+#[cfg(not(target_arch = "wasm32"))]
+use tokio::runtime::Runtime;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub struct Context {
+    runtime: Option<Box<Runtime>>,
+    networking: Box<RNetworking>,
+}
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub struct Context {
+    networking: Box<RNetworking>,
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl Context {
+    pub fn new(cfg: &NetworkingConfig) -> Box<Context> {
+        let rt = Box::new(Runtime::new().unwrap());
+        let mut nt = rt.block_on(async {
+            Box::new(Context {
+                runtime: None,
+                networking: Box::new(RNetworking::new(cfg).unwrap()),
+            })
+        });
+        nt.runtime = Some(rt);
+        nt
+    }
+
+    pub fn send(self: &mut Self, msg: &str) {
+        let nt = self.networking.as_mut();
+        self.runtime.as_mut().unwrap().block_on(async {
+            nt.send_message(msg.into());
+        });
+    }
+
+    pub fn poll(self: &mut Self) -> Vec<String> {
+        let nt = self.networking.as_mut();
+        self.runtime.as_mut().unwrap().block_on(async {
+            nt.poll_messages().into_iter().map(|msg| String::from_utf8(msg).unwrap()).collect()
+        })
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+impl Context {
+    #[wasm_bindgen(constructor)]
+    pub fn new(bootstrap_nodes: Vec<String>, relay_nodes: Vec<String>, private_key: String, enable_kdht: bool, name: String) -> Context {
+        Context {
+            networking: Box::new(network::RNetworking::new(&NetworkingConfig{
+                port: 0,
+                bootstrap_nodes: bootstrap_nodes.clone(),
+                enable_relay_server: false,
+                enable_kdht: enable_kdht,
+                enable_mdns: false,
+                relay_nodes: relay_nodes.clone(),
+                private_key: private_key.clone(),
+                private_key_path: "".to_string(),
+                name: name.clone(),
+                node_types: vec![],
+                node_capabilities: vec![],
+            }).unwrap()),
+        }
+    }
+}
--- networking/src/lib.rs
+++ networking/src/lib.rs
@@ -1,9 +1,11 @@
 mod context;
 use context::Context;
+mod network;
+use network::NetworkingConfig;
 
 #[no_mangle]
-pub extern "C" fn psm_posemesh_networking_context_create() -> *mut Context {
-    let context = Box::new(Context { });
+pub extern "C" fn psm_posemesh_networking_context_create(cfg: &NetworkingConfig) -> *mut Context {
+    let context = Context::new(cfg);
     Box::into_raw(context)
 }
 
