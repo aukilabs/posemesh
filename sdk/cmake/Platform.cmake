df72a6500f2a8a59a44c8e28c5ca4effaa3ea527
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-10-14 11:39:39 +0200

    chore: enable Objective-C to Swift bridging

--- sdk/cmake/Platform.cmake
+++ sdk/cmake/Platform.cmake
@@ -6,5 +6,6 @@ else()
 endif()
 
 # Platform functions
-include(AddPlatformLibrary)
-include(AddPlatformSources)
+include("${CMAKE_CURRENT_LIST_DIR}/AddPlatformLibrary.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/AddPlatformSources.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/SetApplePlatformBridgingHeader.cmake")
