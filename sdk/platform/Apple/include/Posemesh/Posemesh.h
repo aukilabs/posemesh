80a27fc8ebeb5b6819da26e603bd487f5b9024ba
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-10-14 11:39:39 +0200

    chore: enable Objective-C to Swift bridging

--- sdk/platform/Apple/include/Posemesh/Posemesh.h
+++ sdk/platform/Apple/include/Posemesh/Posemesh.h
@@ -2,7 +2,7 @@
 
 #import "Config.h"
 
-NS_SWIFT_NAME(Posemesh.Posemesh) PSM_API @interface PSMPosemesh : NSObject
+NS_SWIFT_NAME(Posemesh) PSM_API @interface PSMPosemesh : NSObject
 
 - (instancetype)init;
 - (void)dealloc;
