ae7f13d36168a39234dc03e5638767d5174cb3b6
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-10-21 22:07:10 +0200

    chore: refine Rust API

--- networking/src/lib.rs
+++ networking/src/lib.rs
@@ -1,9 +1,16 @@
-use std::ffi::c_void;
+mod context;
+use context::Context;
 
 #[no_mangle]
-pub extern "C" fn psm_posemesh_networking_context_create() -> *mut c_void {
+pub extern "C" fn psm_posemesh_networking_context_create() -> *mut Context {
+    let context = Box::new(Context { });
+    Box::into_raw(context)
 }
 
 #[no_mangle]
-pub extern "C" fn psm_posemesh_networking_context_destroy(context: *mut c_void) {
+pub extern "C" fn psm_posemesh_networking_context_destroy(context: *mut Context) {
+    assert!(!context.is_null(), "psm_posemesh_networking_context_destroy(): context is null");
+    unsafe {
+        let _ = Box::from_raw(context);
+    }
 }
