99bd24c51470a102d7069a443c548674749f7004
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-10-28 22:50:00 +0100

    chore: cleanup 1

--- networking/src/lib.rs
+++ networking/src/lib.rs
@@ -1,6 +1,6 @@
-mod network;
 mod client;
 mod context;
+mod network;
 
 use context::{Config, Context};
 use std::ptr::null_mut;
@@ -36,16 +36,31 @@ pub fn posemeshNetworkingContextCreate(config: &Config) -> *mut Context {
     posemesh_networking_context_create(config)
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-#[no_mangle]
-pub extern "C" fn psm_posemesh_networking_context_destroy(context: *mut Context) {
-    assert!(!context.is_null(), "psm_posemesh_networking_context_destroy(): context is null");
+// *******************************************
+// ** posemesh_networking_context_destroy() **
+// *******************************************
+
+fn posemesh_networking_context_destroy(context: *mut Context) {
+    assert!(!context.is_null(), "posemesh_networking_context_destroy(): context is null");
     unsafe {
         let _ = Box::from_raw(context);
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
+#[no_mangle]
+pub extern "C" fn psm_posemesh_networking_context_destroy(context: *mut Context) {
+    posemesh_networking_context_destroy(context);
+}
+
+#[cfg(any(target_arch = "wasm32", target_arch = "wasm64"))]
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn posemeshNetworkingContextDestroy(context: *mut Context) {
+    posemesh_networking_context_destroy(context);
+}
+
+#[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
 #[no_mangle]
 pub extern "C" fn psm_posemesh_networking_send_message(context: *mut Context, msg: Vec<u8>, peer_id: String, protocol: String, callback: extern "C" fn(i32)) {
     assert!(!context.is_null(), "psm_posemesh_networking_send_message(): context is null");
