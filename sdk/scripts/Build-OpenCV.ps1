ef7185f2055857f4d2fd15ea70370bc93e67cfee
Alexander Popov <alex@aukilabs.com>
2024-11-28 17:25:16 +0200

    feat: building OpenCV for iOS as a static framework, repacking the framework as a normal static library, linking static library to posemesh SDK

--- sdk/scripts/Build-OpenCV.ps1
+++ sdk/scripts/Build-OpenCV.ps1
@@ -3,70 +3,93 @@
 Param(
     [Parameter(Position = 0)]
     [ArgumentCompleter({
-        $PossibleValues = @('macOS', 'Mac-Catalyst', 'iOS', 'iOS-Simulator', 'Web')
-        return $PossibleValues | ForEach-Object { $_ }
-    })]
+            $PossibleValues = @('macOS', 'Mac-Catalyst', 'iOS', 'iOS-Simulator', 'Web')
+            return $PossibleValues | ForEach-Object { $_ }
+        })]
     [String]$Platform,
 
     [Parameter(Position = 1)]
     [ArgumentCompleter({
-        $PossibleValues = @('AMD64', 'ARM64', 'WASM32')
-        return $PossibleValues | ForEach-Object { $_ }
-    })]
+            $PossibleValues = @('AMD64', 'ARM64', 'WASM32')
+            return $PossibleValues | ForEach-Object { $_ }
+        })]
     [String]$Architecture
 )
 
-If(-Not $Platform) {
+If (-Not $Platform) {
     Write-Error -Message "Parameter '-Platform' is not specified."
     Exit 1
 }
-If(-Not $Architecture) {
+If (-Not $Architecture) {
     Write-Error -Message "Parameter '-Architecture' is not specified."
     Exit 1
 }
 
-Push-Location -Path "sdk/"
-Write-Host "Cloning OpenCV..."
-
-# Ensure the "third-party" directory exists
-if (-Not (Test-Path "third-party")) {
-    New-Item -ItemType Directory -Path "third-party" | Out-Null
-}
-Push-Location -Path "third-party/"
-
-# Clone the OpenCV repository
-git clone git@github.com:opencv/opencv.git
-Push-Location -Path "opencv/"
-
-Write-Host "Building OpenCV..."
-if ($Platform -eq "iOS") {
-    # iOS building requires a virtual environment
+function Build-iOS {
+    # Python virtual environment required to build on a M1 Mac
+    # https://github.com/opencv/opencv/issues/21926#issuecomment-1184684648
     python3 -m venv .venv
     $env:VIRTUAL_ENV = (Resolve-Path ./.venv).Path
     $env:PATH = "$env:VIRTUAL_ENV/bin" + [System.IO.Path]::PathSeparator + $env:PATH
     python ../opencv/platforms/ios/build_framework.py ios --iphoneos_archs $Architecture --build_only_specified_archs
-} ElseIf ($Platform -eq "macOS") {
-    # Ensure the "build" directory exists
+
+    # Re-pack the framework as a normal static library
+    $sourcePath = "../opencv/ios/opencv2.framework/Versions/A"
+    $destinationPath = "../opencv/opencv-static-lib"
+    $headersPath = Join-Path $sourcePath "Headers"
+    $opencv2Path = Join-Path $sourcePath "opencv2"
+
+    if (-Not (Test-Path $destinationPath)) {
+        New-Item -ItemType Directory -Path $destinationPath | Out-Null
+    }
+
+    $opencv2Dest = Join-Path $destinationPath "opencv2.a"
+    Copy-Item -Path $opencv2Path -Destination $opencv2Dest -Recurse -Force
+
+    $headersDest = Join-Path $destinationPath "opencv2"
+    if (-Not (Test-Path $headersDest)) {
+        New-Item -ItemType Directory -Path $headersDest | Out-Null
+    }
+    Copy-Item -Path (Join-Path $headersPath "*") -Destination $headersDest -Recurse -Force
+}
+
+function Build-MacOS {
     if (-Not (Test-Path "build")) {
         New-Item -ItemType Directory -Path "build" | Out-Null
     }
     Push-Location -Path "build/"
 
-    # Run cmake and build commands
-    Write-Host "MacOS!"
     cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_OPENEXR=OFF -DWITH_OPENEXR=OFF -DCMAKE_INSTALL_PREFIX="./install"
-
     cmake --build . --parallel 8
     make -j 8 install
 
     Pop-Location # build/
-} ElseIf ($Platform -eq "web") {
+}
+
+function Build-Web {
     Write-Host "TODO!"
 }
 
-Write-Host "OpenCV build complete"
+Push-Location -Path "sdk/"
 
-# Return to previous directories
+if (-Not (Test-Path "third-party")) {
+    New-Item -ItemType Directory -Path "third-party" | Out-Null
+}
+Push-Location -Path "third-party/"
+
+git clone git@github.com:opencv/opencv.git
+Push-Location -Path "opencv/"
+
+if ($Platform -eq "iOS") {
+    Build-iOS
+}
+ElseIf ($Platform -eq "macOS") {
+    Build-MacOS
+}
+ElseIf ($Platform -eq "web") {
+    Build-Web
+}
+
 Pop-Location # opencv/
 Pop-Location # third-party/
 Pop-Location # sdk/
