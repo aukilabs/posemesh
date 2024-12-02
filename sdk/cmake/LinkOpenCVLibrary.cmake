55241b0e9971f2deb0fb6e776523e252f4c8acdd
Alexander Popov <alex@aukilabs.com>
2024-12-02 12:50:22 +0200

    chore: added TODO for link_platform_libraries()

--- sdk/cmake/LinkOpenCVLibrary.cmake
+++ sdk/cmake/LinkOpenCVLibrary.cmake
@@ -9,6 +9,15 @@ function(LINK_OPENCV_LIBRARY NAME)
             set(OpenCV_DIR "${CMAKE_CURRENT_LIST_DIR}/third-party/opencv/build/install/lib/cmake/opencv4")
             find_package(OpenCV)
             target_link_libraries(${NAME} PRIVATE ${OpenCV_LIBS})
+
+            # TODO: Use link_platform_libraries instead of target_link_libraries
+            # include_directories(${OpenCV_INCLUDE_DIRS})
+            # link_platform_libraries(
+            #     ${NAME}
+            #     # HIDE_SYMBOLS
+            #     PRIVATE
+            #     "${OpenCV_LIBS}"
+            # )
         endif()
     endif()
 endfunction()
