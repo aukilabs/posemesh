e440da15e6c74dc76d27d5f890875d63d2b81148
jnkvca <122721263+jnkvca@users.noreply.github.com>
2024-12-04 14:52:07 +0100

    chore: add script error handling and missing platforms except web

--- sdk/scripts/Build-OpenCV.ps1
+++ sdk/scripts/Build-OpenCV.ps1
@@ -3,25 +3,28 @@
 Param(
     [Parameter(Position = 0)]
     [ArgumentCompleter({
-            $PossibleValues = @('macOS', 'Mac-Catalyst', 'iOS', 'iOS-Simulator', 'Web')
-            return $PossibleValues | ForEach-Object { $_ }
-        })]
+        $PossibleValues = @('macOS', 'Mac-Catalyst', 'iOS', 'iOS-Simulator', 'Web')
+        return $PossibleValues | ForEach-Object { $_ }
+    })]
     [String]$Platform,
 
     [Parameter(Position = 1)]
     [ArgumentCompleter({
-            $PossibleValues = @('AMD64', 'ARM64', 'WASM32')
-            return $PossibleValues | ForEach-Object { $_ }
-        })]
-    [String]$Architecture
+        $PossibleValues = @('AMD64', 'ARM64', 'WASM32')
+        return $PossibleValues | ForEach-Object { $_ }
+    })]
+    [String]$Architecture,
+
+    [Parameter(Position = 2)]
+    [ArgumentCompleter({
+        $PossibleValues = @('Debug', 'Release')
+        return $PossibleValues | ForEach-Object { $_ }
+    })]
+    [String]$BuildType
 )
 
-If (-Not $Platform) {
+If(-Not $Platform) {
     Write-Error -Message "Parameter '-Platform' is not specified."
-    Exit 1
-}
-If (-Not $Architecture) {
-    Write-Error -Message "Parameter '-Architecture' is not specified."
     Exit 1
 }
 
@@ -49,68 +52,271 @@ function ConvertTo-Framework {
     Copy-Item -Path (Join-Path $headersPath "*") -Destination $headersDest -Recurse -Force
 }
 
-function Build-iOS {
-    # Python virtual environment required to build on a M1 Mac
-    # https://github.com/opencv/opencv/issues/21926#issuecomment-1184684648
-    python3 -m venv .venv
-    $env:VIRTUAL_ENV = (Resolve-Path ./.venv).Path
-    $env:PATH = "$env:VIRTUAL_ENV/bin" + [System.IO.Path]::PathSeparator + $env:PATH
-    python ../opencv/platforms/ios/build_framework.py ios --iphoneos_archs $Architecture --build_only_specified_archs
-
-    ConvertTo-Framework -SourcePath "../opencv/ios/opencv2.framework/Versions/A" -DestinationPath "../opencv/opencv-static-lib"
-}
-
-function Build-iOS-Simulator {
-    # Python virtual environment required to build on a M1 Mac
-    # https://github.com/opencv/opencv/issues/21926#issuecomment-1184684648
-    python3 -m venv .venv
-    $env:VIRTUAL_ENV = (Resolve-Path ./.venv).Path
-    $env:PATH = "$env:VIRTUAL_ENV/bin" + [System.IO.Path]::PathSeparator + $env:PATH
-    python ../opencv/platforms/ios/build_framework.py iphonesimulator --iphonesimulator_archs $Architecture --build_only_specified_archs
-
-    ConvertTo-Framework -SourcePath "../opencv/iphonesimulator/opencv2.framework/Versions/A" -DestinationPath "../opencv/opencv-static-lib-simulator"
-}
-
-function Build-MacOS {
-    if (-Not (Test-Path "build")) {
-        New-Item -ItemType Directory -Path "build" | Out-Null
+$UseCMakeDirectly = $False
+$CMakeGenerator = $Null
+$CMakeConfigureArgs = $Null
+$UseBuildPythonScript = $False
+$BuildPythonScriptFile = $Null
+$BuildPythonScriptArgs = $Null
+Switch($Platform) {
+    'macOS' {
+        If(-Not $IsMacOS) {
+            Write-Error -Message "Your machine needs to be running macOS to build for 'macOS' platform."
+            Exit 1
+        }
+        If(-Not $Architecture) {
+            Write-Error -Message "Parameter '-Architecture' is not specified for 'macOS' platform."
+            Exit 1
+        }
+        $UseBuildPythonScript = $True
+        $BuildPythonScriptFile = 'opencv/platforms/osx/build_framework.py'
+        $BuildPythonScriptArgs = @('--macos_archs')
+        Switch($Architecture) {
+            'AMD64' { $BuildPythonScriptArgs += 'x86_64' }
+            'ARM64' { $BuildPythonScriptArgs += 'arm64' }
+            Default {
+                Write-Error -Message "Invalid or unsupported '$Architecture' architecture for 'macOS' platform."
+                Exit 1
+            }
+        }
+        $BuildPythonScriptArgs += '--build_only_specified_archs'
     }
-    Push-Location -Path "build/"
-
-    cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_OPENEXR=OFF -DWITH_OPENEXR=OFF -DCMAKE_INSTALL_PREFIX="./install"
-    cmake --build . --parallel 8
-    make -j 8 install
-
-    Pop-Location # build/
+    'Mac-Catalyst' {
+        If(-Not $IsMacOS) {
+            Write-Error -Message "Your machine needs to be running macOS to build for 'Mac-Catalyst' platform."
+            Exit 1
+        }
+        If(-Not $Architecture) {
+            Write-Error -Message "Parameter '-Architecture' is not specified for 'Mac-Catalyst' platform."
+            Exit 1
+        }
+        $UseBuildPythonScript = $True
+        $BuildPythonScriptFile = 'opencv/platforms/osx/build_framework.py'
+        $BuildPythonScriptArgs = @('--catalyst_archs')
+        Switch($Architecture) {
+            'AMD64' { $BuildPythonScriptArgs += 'x86_64' }
+            'ARM64' { $BuildPythonScriptArgs += 'arm64' }
+            Default {
+                Write-Error -Message "Invalid or unsupported '$Architecture' architecture for 'Mac-Catalyst' platform."
+                Exit 1
+            }
+        }
+        $BuildPythonScriptArgs += '--build_only_specified_archs'
+    }
+    'iOS' {
+        If(-Not $IsMacOS) {
+            Write-Error -Message "Your machine needs to be running macOS to build for 'iOS' platform."
+            Exit 1
+        }
+        If(-Not $Architecture) {
+            $Architecture = 'ARM64'
+            Write-Warning -Message "Using the implicit '$Architecture' architecture for 'iOS' platform."
+        } ElseIf($Architecture -Ne 'ARM64') {
+            Write-Error -Message "Invalid or unsupported '$Architecture' architecture for 'iOS' platform."
+            Exit 1
+        }
+        $UseBuildPythonScript = $True
+        $BuildPythonScriptFile = 'opencv/platforms/ios/build_framework.py'
+        $BuildPythonScriptArgs = @('--iphoneos_archs', 'arm64', '--build_only_specified_archs')
+    }
+    'iOS-Simulator' {
+        If(-Not $IsMacOS) {
+            Write-Error -Message "Your machine needs to be running macOS to build for 'iOS-Simulator' platform."
+            Exit 1
+        }
+        If(-Not $Architecture) {
+            Write-Error -Message "Parameter '-Architecture' is not specified for 'iOS-Simulator' platform."
+            Exit 1
+        }
+        $UseBuildPythonScript = $True
+        $BuildPythonScriptFile = 'opencv/platforms/ios/build_framework.py'
+        $BuildPythonScriptArgs = @('--iphonesimulator_archs')
+        Switch($Architecture) {
+            'AMD64' { $BuildPythonScriptArgs += 'x86_64' }
+            'ARM64' { $BuildPythonScriptArgs += 'arm64' }
+            Default {
+                Write-Error -Message "Invalid or unsupported '$Architecture' architecture for 'iOS-Simulator' platform."
+                Exit 1
+            }
+        }
+        $BuildPythonScriptArgs += '--build_only_specified_archs'
+    }
+    'Web' {
+        If(-Not $Architecture) {
+            $Architecture = 'WASM32'
+            Write-Warning -Message "Using the implicit '$Architecture' architecture for 'Web' platform."
+        } ElseIf($Architecture -Ne 'WASM32') {
+            Write-Error -Message "Invalid or unsupported '$Architecture' architecture for 'Web' platform."
+            Exit 1
+        }
+        # TODO
+    }
+    Default {
+        Write-Error -Message "Invalid or unsupported '$Platform' platform."
+        Exit 1
+    }
+}
+If($UseCMakeDirectly) {
+    If($UseBuildPythonScript) {
+        Write-Error -Message 'ASSERT: Both variables $UseCMakeDirectly and $UseBuildPythonScript cannot be set to $True simultaneously.'
+        Exit 1
+    }
+    If($CMakeGenerator -Eq $Null) {
+        Write-Error -Message 'ASSERT: Variable $CMakeGenerator is not set.'
+        Exit 1
+    }
+    If($CMakeConfigureArgs -Eq $Null) {
+        Write-Error -Message 'ASSERT: Variable $CMakeConfigureArgs is not set.'
+        Exit 1
+    }
+} ElseIf($UseBuildPythonScript) {
+    If($BuildPythonScriptFile -Eq $Null) {
+        Write-Error -Message 'ASSERT: Variable $BuildPythonScriptFile is not set.'
+        Exit 1
+    }
+    If($BuildPythonScriptArgs -Eq $Null) {
+        Write-Error -Message 'ASSERT: Variable $BuildPythonScriptArgs is not set.'
+        Exit 1
+    }
+} Else {
+    Write-Error -Message 'ASSERT: Either variable $UseCMakeDirectly or $UseBuildPythonScript must be set to $True.'
+    Exit 1
 }
 
-function Build-Web {
-    Write-Host "TODO!"
+$CMakeCommand = (Get-Command -Name 'cmake') 2> $Null
+If(-Not $CMakeCommand) {
+    Write-Error -Message "Could not find 'cmake' command. Is CMake installed on your machine?"
+    Exit 1
 }
 
-Push-Location -Path "sdk/"
-
-if (-Not (Test-Path "third-party")) {
-    New-Item -ItemType Directory -Path "third-party" | Out-Null
-}
-Push-Location -Path "third-party/"
-
-git clone git@github.com:opencv/opencv.git
-Push-Location -Path "opencv/"
-
-if ($Platform -eq "iOS") {
-    Build-iOS
-}
-if ($Platform -eq "iOS-Simulator") {
-    Build-iOS-Simulator
-}
-ElseIf ($Platform -eq "macOS") {
-    Build-MacOS
-}
-ElseIf ($Platform -eq "web") {
-    Build-Web
+$XcodeBuildCommand = $Null
+If(($UseCMakeDirectly -And ($CMakeGenerator -Eq 'Xcode')) -Or $UseBuildPythonScript) {
+    $XcodeBuildCommand = (Get-Command -Name 'xcodebuild') 2> $Null
+    If(-Not $XcodeBuildCommand) {
+        Write-Error -Message "Could not find 'xcodebuild' command. Is Xcode installed on your machine?"
+        Exit 1
+    }
 }
 
-Pop-Location # opencv/
-Pop-Location # third-party/
-Pop-Location # sdk/
+$Python3Command = $Null
+If($UseBuildPythonScript) {
+    $Python3Command = (Get-Command -Name 'python3') 2> $Null
+    If(-Not $Python3Command) {
+        Write-Error -Message "Could not find 'python3' command. Is Python installed on your machine?"
+        Exit 1
+    }
+}
+
+If(-Not $BuildType) {
+    $BuildType = 'Release'
+    Write-Warning -Message "Using the implicit '$BuildType' build type."
+}
+$CMakeBuildTypeFlagForConfiguring = $Null
+$CMakeBuildTypeFlagForBuildingAndInstalling = $Null
+$BuildPythonScriptBuildTypeFlag = $Null
+Switch($BuildType) {
+    'Debug' {
+        $CMakeBuildTypeFlagForConfiguring = '-DCMAKE_BUILD_TYPE=Debug'
+        If($UseCMakeDirectly -And ($CMakeGenerator -Eq 'Xcode')) {
+            $CMakeBuildTypeFlagForBuildingAndInstalling = '--config Debug'
+        } Else {
+            $CMakeBuildTypeFlagForBuildingAndInstalling = ''
+        }
+        $BuildPythonScriptBuildTypeFlag = '--debug'
+    }
+    'Release' {
+        $CMakeBuildTypeFlagForConfiguring = '-DCMAKE_BUILD_TYPE=Release'
+        If($UseCMakeDirectly -And ($CMakeGenerator -Eq 'Xcode')) {
+            $CMakeBuildTypeFlagForBuildingAndInstalling = '--config Release'
+        } Else {
+            $CMakeBuildTypeFlagForBuildingAndInstalling = ''
+        }
+        $BuildPythonScriptBuildTypeFlag = ''
+    }
+    Default {
+        Write-Error -Message "Invalid or unsupported '$BuildType' build type."
+        Exit 1
+    }
+}
+If($CMakeBuildTypeFlagForConfiguring -Eq $Null) {
+    Write-Error -Message 'ASSERT: Variable $CMakeBuildTypeFlagForConfiguring is not set.'
+    Exit 1
+}
+If($CMakeBuildTypeFlagForBuildingAndInstalling -Eq $Null) {
+    Write-Error -Message 'ASSERT: Variable $CMakeBuildTypeFlagForBuildingAndInstalling is not set.'
+    Exit 1
+}
+If($BuildPythonScriptBuildTypeFlag -Eq $Null) {
+    Write-Error -Message 'ASSERT: Variable $BuildPythonScriptBuildTypeFlag is not set.'
+    Exit 1
+}
+
+$PushLocationResult = (Push-Location -Path "$PSScriptRoot/../../third-party" -PassThru) 2> $Null
+If(-Not $PushLocationResult) {
+    Write-Error -Message 'Failed to push the required working directory.'
+    Exit 1
+}
+Try {
+    $DirectoryNameSuffix = "OpenCV-$Platform-$Architecture-$BuildType"
+    $BuildDirectoryName = "build-$DirectoryNameSuffix"
+    $OutDirectoryName = "out-$DirectoryNameSuffix"
+    If(-Not (Test-Path -Path $BuildDirectoryName -PathType Container)) {
+        $NewItemResult = (New-Item -Path . -Name $BuildDirectoryName -ItemType Directory) 2> $Null
+        If(-Not $NewItemResult) {
+            Write-Error -Message "Failed to create '$BuildDirectoryName' directory."
+            Exit 1
+        }
+    }
+    If(-Not (Test-Path -Path $OutDirectoryName -PathType Container)) {
+        $NewItemResult = (New-Item -Path . -Name $OutDirectoryName -ItemType Directory) 2> $Null
+        If(-Not $NewItemResult) {
+            Write-Error -Message "Failed to create '$OutDirectoryName' directory."
+            Exit 1
+        }
+    }
+
+    If($UseCMakeDirectly) {
+        $CMakeGeneratorFlags = @()
+        If($CMakeGenerator -Ne '') {
+            $CMakeGeneratorFlags = @('-G', $CMakeGenerator)
+        }
+        & $CMakeCommand $CMakeGeneratorFlags $CMakeConfigureArgs @($CMakeBuildTypeFlagForConfiguring | Where-Object { $_ }) "-DCMAKE_INSTALL_PREFIX=$OutDirectoryName" -B $BuildDirectoryName -S opencv
+        If($LastExitCode -Ne 0) {
+            Write-Error -Message 'Failed to configure CMake project.'
+            Exit 1
+        }
+        & $CMakeCommand --build $BuildDirectoryName @($CMakeBuildTypeFlagForBuildingAndInstalling | Where-Object { $_ })
+        If($LastExitCode -Ne 0) {
+            Write-Error -Message 'Failed to build OpenCV library.'
+            Exit 1
+        }
+        & $CMakeCommand --install $BuildDirectoryName @($CMakeBuildTypeFlagForBuildingAndInstalling | Where-Object { $_ })
+        If($LastExitCode -Ne 0) {
+            Write-Error -Message 'Failed to install CMake project.'
+            Exit 1
+        }
+    } ElseIf($UseBuildPythonScript) {
+        & $Python3Command -m venv "$BuildDirectoryName/.venv"
+        $VirtualEnvBackup = $env:VIRTUAL_ENV
+        $PathBackup = $env:PATH
+        Try {
+            $env:VIRTUAL_ENV = (Resolve-Path "$BuildDirectoryName/.venv").Path
+            If($env:PATH) {
+                $env:PATH = "$env:VIRTUAL_ENV/bin" + [System.IO.Path]::PathSeparator + $env:PATH
+            } Else {
+                $env:PATH = "$env:VIRTUAL_ENV/bin"
+            }
+            $Python3CommandFromVEnv = (Get-Command -Name 'python3') 2> $Null
+            If((-Not $Python3CommandFromVEnv) -Or (-Not $Python3CommandFromVEnv.Source.StartsWith($env:VIRTUAL_ENV))) {
+                Write-Error -Message "Could not find 'python3' command in Python virtual environment."
+                Exit 1
+            }
+            & $Python3CommandFromVEnv $BuildPythonScriptFile $BuildDirectoryName $BuildPythonScriptArgs @($BuildPythonScriptBuildTypeFlag | Where-Object { $_ })
+        } Finally {
+            $env:VIRTUAL_ENV = $VirtualEnvBackup
+            $env:PATH = $PathBackup
+        }
+    }
+} Finally {
+    Pop-Location
+}
