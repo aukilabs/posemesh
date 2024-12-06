9924c11ebca5a2a4caf9469dfcb2af0580881833
Alexander <sasoh@users.noreply.github.com>
2024-12-06 14:48:57 +0200

    feat: OpenCV dependency (#18)
    * feat: added a build script that clones, builds statically and links OpenCV
    
    * chore: replaced bash script with a ps one; building OpenCV for macOS & iOS platforms
    
    * feat: building OpenCV for iOS as a static framework, repacking the framework as a normal static library, linking static library to posemesh SDK
    
    * chore: extracted OpenCV linking to a separate function
    
    * chore: added TODO for link_platform_libraries()
    
    * feat: building for ios simulator
    
    * chore: add script error handling and missing platforms except web
    
    * chore: add Unpack-Framework function
    
    * chore: add Build-OpenCV-Apple.ps1
    
    * chore: add OpenCV Emscripten build
    
    * chore: linking
    
    * chore: web build
    
    * chore: add Get-OpenCV-CacheHash.ps1
    
    * fix: Babel.js
    
    ---------
    
    Co-authored-by: jnkvca <122721263+jnkvca@users.noreply.github.com>

---
+++ sdk/cmake/LinkOpenCVLibrary.cmake
@@ -0,0 +1,98 @@
+include("${CMAKE_CURRENT_LIST_DIR}/GetBuildDirectorySuffix.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/LinkPlatformLibraries.cmake")
+
+set(THIRD_PARTY_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../third-party")
+
+function(LINK_OPENCV_LIBRARY NAME)
+    if(NOT TARGET ${NAME})
+        message(FATAL_ERROR "Target '${NAME}' does not exist.")
+    endif()
+
+    get_build_directory_suffix(BUILD_DIRECTORY_SUFFIX)
+    set(OPENCV_OUTPUT_DIRECTORY "${THIRD_PARTY_PREFIX}/out-OpenCV-${BUILD_DIRECTORY_SUFFIX}")
+    set(OPENCV_INCLUDE_DIRECTORY "${OPENCV_OUTPUT_DIRECTORY}/include")
+    set(OPENCV_LIBRARY_DIRECTORY "${OPENCV_OUTPUT_DIRECTORY}/lib")
+
+    if(NOT EXISTS "${OPENCV_INCLUDE_DIRECTORY}" OR NOT IS_DIRECTORY "${OPENCV_INCLUDE_DIRECTORY}")
+        message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Includes directory is missing.")
+    endif()
+    target_include_directories(
+        ${NAME}
+        PRIVATE
+            ${OPENCV_INCLUDE_DIRECTORY}
+    )
+
+    if(EMSCRIPTEN)
+        set(OPENCV_CALIB3D_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_calib3d.a")
+        if(NOT EXISTS "${OPENCV_CALIB3D_LIBRARY}" OR IS_DIRECTORY "${OPENCV_CALIB3D_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_calib3d.a' is missing.")
+        endif()
+
+        set(OPENCV_CORE_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_core.a")
+        if(NOT EXISTS "${OPENCV_CORE_LIBRARY}" OR IS_DIRECTORY "${OPENCV_CORE_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_core.a' is missing.")
+        endif()
+
+        set(OPENCV_DNN_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_dnn.a")
+        if(NOT EXISTS "${OPENCV_DNN_LIBRARY}" OR IS_DIRECTORY "${OPENCV_DNN_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_dnn.a' is missing.")
+        endif()
+
+        set(OPENCV_FEATURES2D_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_features2d.a")
+        if(NOT EXISTS "${OPENCV_FEATURES2D_LIBRARY}" OR IS_DIRECTORY "${OPENCV_FEATURES2D_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_features2d.a' is missing.")
+        endif()
+
+        set(OPENCV_FLANN_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_flann.a")
+        if(NOT EXISTS "${OPENCV_FLANN_LIBRARY}" OR IS_DIRECTORY "${OPENCV_FLANN_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_flann.a' is missing.")
+        endif()
+
+        set(OPENCV_IMGPROC_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_imgproc.a")
+        if(NOT EXISTS "${OPENCV_IMGPROC_LIBRARY}" OR IS_DIRECTORY "${OPENCV_IMGPROC_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_imgproc.a' is missing.")
+        endif()
+
+        set(OPENCV_OBJDETECT_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_objdetect.a")
+        if(NOT EXISTS "${OPENCV_OBJDETECT_LIBRARY}" OR IS_DIRECTORY "${OPENCV_OBJDETECT_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_objdetect.a' is missing.")
+        endif()
+
+        set(OPENCV_PHOTO_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_photo.a")
+        if(NOT EXISTS "${OPENCV_PHOTO_LIBRARY}" OR IS_DIRECTORY "${OPENCV_PHOTO_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_photo.a' is missing.")
+        endif()
+
+        set(OPENCV_VIDEO_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv_video.a")
+        if(NOT EXISTS "${OPENCV_VIDEO_LIBRARY}" OR IS_DIRECTORY "${OPENCV_VIDEO_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file 'libopencv_video.a' is missing.")
+        endif()
+
+        link_platform_libraries(
+            ${NAME}
+            HIDE_SYMBOLS
+            PRIVATE
+                "${OPENCV_CALIB3D_LIBRARY}"
+                "${OPENCV_CORE_LIBRARY}"
+                "${OPENCV_DNN_LIBRARY}"
+                "${OPENCV_FEATURES2D_LIBRARY}"
+                "${OPENCV_FLANN_LIBRARY}"
+                "${OPENCV_IMGPROC_LIBRARY}"
+                "${OPENCV_OBJDETECT_LIBRARY}"
+                "${OPENCV_PHOTO_LIBRARY}"
+                "${OPENCV_VIDEO_LIBRARY}"
+        )
+    else()
+        set(OPENCV_LIBRARY "${OPENCV_LIBRARY_DIRECTORY}/libopencv2.a")
+        if(NOT EXISTS "${OPENCV_LIBRARY}" OR IS_DIRECTORY "${OPENCV_LIBRARY}")
+            message(FATAL_ERROR "OpenCV library is not built for targeted platform, architecture and configuration (build type): Archive file is missing.")
+        endif()
+
+        link_platform_libraries(
+            ${NAME}
+            HIDE_SYMBOLS
+            PRIVATE
+                "${OPENCV_LIBRARY}"
+        )
+    endif()
+endfunction()
