c25f52934ec7c8505a7655d7781540781730820e
Alexander <sasoh@users.noreply.github.com>
2025-03-04 04:13:01 +0100

    chore: updated GLM header include script to use library name as a parameter

--- sdk/cmake/AddGLMHeaders.cmake
+++ sdk/cmake/AddGLMHeaders.cmake
@@ -1,3 +1,6 @@
-function(ADD_GLM_HEADERS)
-    target_include_directories(Posemesh PRIVATE "${CMAKE_CURRENT_LIST_DIR}/../third-party/glm")
+function(ADD_GLM_HEADERS NAME)
+    if(NOT TARGET ${NAME})
+        message(FATAL_ERROR "Target '${NAME}' does not exist.")
+    endif()
+    target_include_directories(${NAME} PRIVATE "${CMAKE_CURRENT_LIST_DIR}/../third-party/glm")
 endfunction()
