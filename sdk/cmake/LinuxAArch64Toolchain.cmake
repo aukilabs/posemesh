d5526272ddcf04787cc1bc74ec019c5ec168b344
jnkvca <122721263+jnkvca@users.noreply.github.com>
2025-03-14 19:12:18 +0100

    attempt

--- sdk/cmake/LinuxAArch64Toolchain.cmake
+++ sdk/cmake/LinuxAArch64Toolchain.cmake
@@ -5,6 +5,3 @@ set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 
 set(CMAKE_SYSROOT "/usr/aarch64-linux-gnu")
-set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu")
-
-set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
