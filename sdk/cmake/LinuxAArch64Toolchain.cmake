fb58513e9ed69eaf8a2cbda7def318de06c60e45
jnkvca <122721263+jnkvca@users.noreply.github.com>
2025-03-14 14:03:55 +0100

    fix

--- sdk/cmake/LinuxAArch64Toolchain.cmake
+++ sdk/cmake/LinuxAArch64Toolchain.cmake
@@ -1,7 +1,10 @@
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
-set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
+set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 
 set(CMAKE_SYSROOT "/usr/aarch64-linux-gnu")
+set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu")
+
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
