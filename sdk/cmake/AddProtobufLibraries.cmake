21e4f4f77d1c6527388d0665d0989cdfb807d766
Alexander Popov <alex@aukilabs.com>
2025-04-09 17:09:59 +0300

    chore: setting prefix & suffixes for libraries in a loop

--- sdk/cmake/AddProtobufLibraries.cmake
+++ sdk/cmake/AddProtobufLibraries.cmake
@@ -1,72 +1,72 @@
 function(ADD_PROTOBUF_LIBRARIES NAME PROTOBUF_LIBRARY_ROOT)
     if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug") 
-        set(PROTOBUF_LIB "libprotobufd.a")    
+        set(PROTOBUF_LIB "protobufd")    
     else()
-        set(PROTOBUF_LIB "libprotobuf.a")
+        set(PROTOBUF_LIB "protobuf")
     endif()
 
     set(PROTOBUF_LIB_AND_DEPENDENCIES
         ${PROTOBUF_LIB}
-        libabsl_base.a
-        libabsl_city.a
-        libabsl_cord.a
-        libabsl_cord_internal.a
-        libabsl_cordz_handle.a
-        libabsl_cordz_info.a
-        libabsl_crc32c.a
-        libabsl_crc_cord_state.a
-        libabsl_crc_cpu_detect.a
-        libabsl_crc_internal.a
-        libabsl_decode_rust_punycode.a
-        libabsl_demangle_internal.a
-        libabsl_demangle_rust.a
-        libabsl_examine_stack.a
-        libabsl_flags_config.a
-        libabsl_flags_internal.a
-        libabsl_flags_reflection.a
-        libabsl_graphcycles_internal.a
-        libabsl_hash.a
-        libabsl_int128.a
-        libabsl_kernel_timeout_internal.a
-        libabsl_leak_check.a
-        libabsl_log_entry.a
-        libabsl_log_flags.a
-        libabsl_log_globals.a
-        libabsl_log_initialize.a
-        libabsl_log_internal_check_op.a
-        libabsl_log_internal_conditions.a
-        libabsl_log_internal_format.a
-        libabsl_log_internal_globals.a
-        libabsl_log_internal_log_sink_set.a
-        libabsl_log_internal_message.a
-        libabsl_log_internal_nullguard.a
-        libabsl_log_internal_proto.a
-        libabsl_log_internal_structured_proto.a
-        libabsl_log_sink.a
-        libabsl_low_level_hash.a
-        libabsl_malloc_internal.a
-        libabsl_raw_hash_set.a
-        libabsl_raw_logging_internal.a
-        libabsl_spinlock_wait.a
-        libabsl_stacktrace.a
-        libabsl_status.a
-        libabsl_statusor.a
-        libabsl_str_format_internal.a
-        libabsl_strerror.a
-        libabsl_strings.a
-        libabsl_strings_internal.a
-        libabsl_symbolize.a
-        libabsl_synchronization.a
-        libabsl_throw_delegate.a
-        libabsl_time.a
-        libabsl_time_zone.a
-        libabsl_utf8_for_code_point.a
-        libabsl_vlog_config_internal.a
-        libutf8_range.a
+        absl_base
+        absl_city
+        absl_cord
+        absl_cord_internal
+        absl_cordz_handle
+        absl_cordz_info
+        absl_crc32c
+        absl_crc_cord_state
+        absl_crc_cpu_detect
+        absl_crc_internal
+        absl_decode_rust_punycode
+        absl_demangle_internal
+        absl_demangle_rust
+        absl_examine_stack
+        absl_flags_config
+        absl_flags_internal
+        absl_flags_reflection
+        absl_graphcycles_internal
+        absl_hash
+        absl_int128
+        absl_kernel_timeout_internal
+        absl_leak_check
+        absl_log_entry
+        absl_log_flags
+        absl_log_globals
+        absl_log_initialize
+        absl_log_internal_check_op
+        absl_log_internal_conditions
+        absl_log_internal_format
+        absl_log_internal_globals
+        absl_log_internal_log_sink_set
+        absl_log_internal_message
+        absl_log_internal_nullguard
+        absl_log_internal_proto
+        absl_log_internal_structured_proto
+        absl_log_sink
+        absl_low_level_hash
+        absl_malloc_internal
+        absl_raw_hash_set
+        absl_raw_logging_internal
+        absl_spinlock_wait
+        absl_stacktrace
+        absl_status
+        absl_statusor
+        absl_str_format_internal
+        absl_strerror
+        absl_strings
+        absl_strings_internal
+        absl_symbolize
+        absl_synchronization
+        absl_throw_delegate
+        absl_time
+        absl_time_zone
+        absl_utf8_for_code_point
+        absl_vlog_config_internal
+        utf8_range
     )
     
     foreach(lib ${PROTOBUF_LIB_AND_DEPENDENCIES})
-        list(APPEND PROTOBUF_DEPENDENCY_LIBS "${PROTOBUF_LIBRARY_ROOT}/lib/${lib}")
+        list(APPEND PROTOBUF_DEPENDENCY_LIBS "${PROTOBUF_LIBRARY_ROOT}/lib/lib${lib}.a")
     endforeach()
 
     set(Protobuf_LIBRARIES ${PROTOBUF_DEPENDENCY_LIBS} CACHE STRING INTERNAL)
