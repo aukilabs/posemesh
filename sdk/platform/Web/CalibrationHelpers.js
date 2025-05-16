9028c3b179e8ccc222920aee56ec3f5766e7b1e6
Alexander Popov <alex@aukilabs.com>
2025-05-16 17:52:52 +0300

    chore: fixed JS binding function name

--- sdk/platform/Web/CalibrationHelpers.js
+++ sdk/platform/Web/CalibrationHelpers.js
@@ -2,7 +2,7 @@ posemeshModule.CalibrationHelpers = null;
 
 __internalPosemeshAPI.builderFunctions.push(function() {
     __internalPosemesh.CalibrationHelpers.getCalibrationMatrix = function(poseInDomain, observedPose, onlyRotateAroundY) {
-        return __internalPosemesh.CalibrationHelpers.__getCalibrationmatrix(poseInDomain, observedPose, onlyRotateAroundY);
+        return __internalPosemesh.CalibrationHelpers.__getCalibrationMatrix(poseInDomain, observedPose, onlyRotateAroundY);
     };
 
     posemeshModule.CalibrationHelpers = __internalPosemesh.CalibrationHelpers;
